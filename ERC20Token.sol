e442814b658bdc42c54560d0b1f31dad6328ee3d
권영경 <86418308+asaei623@users.noreply.github.com>
2023-05-18 00:06:52 +0900

    [FEAT] ERC20Token.sol 생성

---
+++ ERC20Token.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.0;
+
+contract ERC20Token {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    //토큰 생성자
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply * 10 ** uint256(_decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+    
+    //토큰 전송 함수
+    function transfer(address _to, uint256 _value) external returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        
+        emit Transfer(msg.sender, _to, _value);
+        
+        return true;
+    }
+}
